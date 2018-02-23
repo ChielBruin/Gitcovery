fc4e21f600cf7d400545f6550887bb1931b376d5
Chiel Bruin <github@bruin.ch>
2018-02-23 19:44:11 +0100

    Implemented commit log parsing

--- commit.py
+++ commit.py
@@ -1,11 +1,12 @@
+import re, datetime
+
 import gitfs
 from diff import Diff
 
 
 class Commit:
 	_author = ''
-	_authorDate = ''
-	_commit = ''
+	_authorMail = ''
 	_commitDate = ''
 	_title = ''
 	_msg = ''
@@ -18,8 +19,20 @@ class Commit:
 		if self._author:
 			return
 		out = gitfs.Git.call(['log', '-n', '1', self.sha, '--pretty=fuller'])
-		#TODO
-		#print(out)
+		matcher = re.search('Author:\s*(?P<author>.+)\s*<(?P<authorMail>.+@.+)>\n.*\n.*\n' +
+					  'CommitDate:\s*(?P<commitDate>.+)\n\n' +
+					  '\s*(?P<title>.+)(\n\s*\n(?P<message> (.*\n*)+))?', out)
+					
+		if not matcher:
+			raise Exception('git log output could not be matched: ' + out)
+			  
+		self._author     = matcher.group('author')
+		self._authorMail = matcher.group('authorMail')
+		dateString       = matcher.group('commitDate')
+		self._commitDate = datetime.datetime.strptime(dateString, '%a %b %d %H:%M:%S %Y %z').date()
+		self._title      = matcher.group('title')
+		self._msg        = matcher.group('message') if matcher.group('message') else ''
+		
 		self.data = True
 	
 	def getDiff(self) -> None:
@@ -30,12 +43,31 @@ class Commit:
 		#print(out)
 		self._diff = Diff(out)
 		
-	@property
 	def author(self) -> str:
-		if not self.data:
+		if not self._author:
 			self.getData()
 		return self._author
-			
+
+	def authorMail(self) -> str:
+		if not self._authorMail:
+			self.getData()
+		return self._authorMail
+
+	def commitDate(self) -> datetime.date:
+		if not self._commitDate:
+			self.getData()
+		return self._commitDate
+
+	def title(self) -> str:
+		if not self._title:
+			self.getData()
+		return self._title
+
+	def msg(self) -> str:
+		if not self._msg:
+			self.getData()
+		return self._msg
+
 	def changes(self, fileName=None) -> Diff:
 		if not self._diff:
 			self.getDiff()
