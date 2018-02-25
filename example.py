43f8f11f40c9de7be0b5c6506bce2d69f2c40f1f
Chiel Bruin <github@bruin.ch>
2018-02-25 22:31:04 +0100

    Streamlined the example

--- example.py
+++ example.py
@@ -4,10 +4,9 @@ from gitcovery import Git
 folder = Git.setRoot('.')
 #folder = Git.clone('/tmp', 'https://github.com/ChielBruin/Gitcovery.git')
 
-print(folder.get('README.md').status())
 print(folder.get('README.md'))
 
 print(folder.history()[0].changes())
 print(len(folder.history()), len(folder.get('README.md').history()))
 
-folder.forEachFile(lambda x: print(x.path))
+folder.forEachFile(lambda x: print(x.status(), x.path))
