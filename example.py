ae8026233ab1704df4088c1368fcbb09abcda68a
Chiel Bruin <github@bruin.ch>
2018-02-26 12:39:35 +0100

    Imporved example to show usage of Authors

--- example.py
+++ example.py
@@ -6,7 +6,14 @@ folder = Git.setRoot('.')
 
 print(folder.get('README.md'))
 
-print(folder.history()[0].changes())
+history = folder.history()
+print(history[0].changes())
 print(len(folder.history()), len(folder.get('README.md').history()))
 
 folder.forEachFile(lambda x: print(x.status(), x.path))
+
+# Make sure to load all commits before getting author data
+for commit in history:
+	commit.load()
+
+print(len(Git.getAuthor('Chiel Bruin').commits))
