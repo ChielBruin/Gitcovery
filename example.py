c113118ccafa01f114b9dd146293db86f990e7e6
Chiel Bruin <github@bruin.ch>
2018-02-28 20:16:53 +0100

    Added the Diffs to the example

--- example.py
+++ example.py
@@ -10,10 +10,12 @@ print(history[0].changes())
 print(folder.get('README.md').at(history[0]))
 print(len(folder.history()), len(folder.get('README.md').history()))
 
-folder.forEachFile(lambda x: print(x.status(), x.path))
-
-# Make sure to load all commits before getting author data
 for commit in history:
+	d = commit.changes()
+	print('%s: +%d, -%d\t(%d)\t%s'%(commit.sha, d.numAdded(),d.numRemoved(), len(d), commit.title()))
 	commit.load()
 
+# Make sure to load all commits before getting author data
 print(len(Git.getAuthor('Chiel Bruin').commits))
+
+folder.forEachFile(lambda x: print(x.status(), x.path))
