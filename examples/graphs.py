2d740664527ddb5f7f7cd365e112fc768a9472d1
Chiel Bruin <github@bruin.ch>
2018-03-08 15:31:04 +0100

    Moved the example to a dedicated folder and added an example for drawing graphs

---
+++ examples/graphs.py
@@ -0,0 +1,24 @@
+import matplotlib.pyplot as plt
+from gitcovery import Git
+
+'''
+Create a graph that shows the number of lines in the README file on each commit it was involved in.
+'''
+
+root = Git.setRoot('.')
+readmeFile = root.getFile('README.md')
+
+# History is new -> old, we need it the other way around. 
+# Instead of reversing, you could also sort the list to make it chronological
+history = list(reversed(readmeFile.history())) 
+
+# Get the number of newlines in the document at each commit in the history of the file
+sizes = list(map(lambda x: readmeFile.countAt('\n', x), history))
+
+
+# Draw the graph
+plt.plot(sizes)
+plt.ylim(ymin=0)
+plt.legend(['README.md'], loc='upper left')
+plt.show()
+
