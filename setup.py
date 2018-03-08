f13c5f79f581b8f05c2987700190f1539111a469
Chiel Bruin <github@bruin.ch>
2018-03-08 22:49:46 +0100

    Bumped version and fixed github link

--- setup.py
+++ setup.py
@@ -1,9 +1,9 @@
 from setuptools import setup
 
 setup(name='gitcovery',
-      version='0.1',
+      version='0.2',
       description='A wrapper for the git filesystem',
-      url='http://github.com/storborg/funniest',
+      url='http://github.com/ChielBruin/gitcovery',
       author='Chiel Bruin',
       author_email='github@bruin.ch',
       license='',
