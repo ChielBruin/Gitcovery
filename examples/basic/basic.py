e929512d675c732c92c59d90116f857ef466a7d5
Chiel Bruin <github@bruin.ch>
2018-03-18 20:34:44 +0100

    Moved examples and added documentation for them

---
+++ examples/basic/basic.py
@@ -0,0 +1,41 @@
+from __future__ import print_function
+from gitcovery import Git
+
+'''
+A basic example for usage of the module.
+
+In this example most of the core functionalities of the module are shown, including:
+ - Getting the full history of the repository
+ - Getting authors of commits
+ - Getting the number of lines changed in a commit
+ - Getting file contents at a specific point in history
+ - Getting file properties of all files
+'''
+
+#root = Git.set_root('.')
+root = Git.clone('/tmp', 'https://github.com/ChielBruin/Gitcovery.git')
+history = root.history()
+
+# Print the authors of the initial commits
+print('Initial commits by:', ', '.join(map(lambda x: x.author().name, Git.get_initial_commits())))
+
+# Print the number of lines changed in the most recent commit
+print('Lines changed in last commit:', len(history[0].changes()))
+
+# Print the contents of the README file 6 commits ago
+print(root.get_file('README.md').at(history[6]))
+
+# Print the number of commits in both the full history and the history of the README file
+print('The full history contains %d commits, this contains %d changes in the README file.' % (len(root.history()), len(root.get_file('README.md').history())))
+
+# For each commit print the hash, lines added, lines removed, lines changed and title
+for commit in history:
+    diff = commit.changes()
+    print('%s: +%d, -%d\t(%d)\t%s' % (commit.sha, diff.num_added(), diff.num_removed(), len(diff), commit.title()))
+    commit.load()	# Make sure to load all commits before getting author data
+
+# Print the number of commits made by 'Chiel Bruin'
+print(len(Git.get_author('Chiel Bruin').commits))
+
+# Print the status and relative path of each file
+root.for_each_file(lambda x: print(x.status(), x.relative_path))
--- examples/filesizePlot/filesizePlot.py
+++ examples/filesizePlot/filesizePlot.py
@@ -2,10 +2,15 @@ import matplotlib.pyplot as plt
 from gitcovery import Git
 
 '''
-Create a graph that shows the number of lines in the README file on each commit it was involved in.
+Create a graph that shows the number of lines in the README file over time.
+
+For this we first get the commits that have changed the file.
+For each commit in this history we then get the size of the file.
+Lastly, these sizes are displayed in a graph.
 '''
 
-root = Git.set_root('.')
+#root = Git.set_root('.')
+root = Git.clone('/tmp', 'https://github.com/ChielBruin/Gitcovery.git')
 readmeFile = root.get_file('README.md')
 
 # History is new -> old, we need it the other way around. 
@@ -13,11 +18,10 @@ readmeFile = root.get_file('README.md')
 history = list(reversed(readmeFile.history())) 
 
 # Get the number of newlines in the document at each commit in the history of the file
-sizes = list(map(lambda x: readmeFile.count('\n', at=x), history))
-
+fileSizes = list(map(lambda commit: readmeFile.count('\n', at=commit), history))
 
 # Draw the graph
-plt.plot(sizes)
+plt.plot(fileSizes)
 plt.ylim(ymin=0)
 plt.legend(['README.md'], loc='upper left')
 plt.show()
